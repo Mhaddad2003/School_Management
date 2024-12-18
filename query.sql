5fe9576502535fae6bd9e157b0a08977611fa941
MariamChairi <146886902+MariamChairi@users.noreply.github.com>
2024-12-18 11:53:11 +0100

    Gestion_de_Scolarite.sql

--- query.sql
+++ query.sql
@@ -1,5 +1,9 @@
--- CREATE DATABASE IF NOT EXISTS Gestion_Scolarite;
+-- Créer la base de données
+CREATE DATABASE IF NOT EXISTS Gestion_Scolarite;
 
+-- Créer les tables dans SQLite
+
+-- Table Etudiant
 CREATE TABLE IF NOT EXISTS Etudiant (
     num_apogee INTEGER NOT NULL PRIMARY KEY,
     nom TEXT NOT NULL,
@@ -8,6 +12,7 @@ CREATE TABLE IF NOT EXISTS Etudiant (
     date_naiss date NOT NULL,
 );
 
+-- Table Enseignant
 CREATE TABLE IF NOT EXISTS Enseignant (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     nom TEXT NOT NULL,
@@ -16,20 +21,22 @@ CREATE TABLE IF NOT EXISTS Enseignant (
     departement TEXT NOT NULL,
 );
 
+-- Table Module
 CREATE TABLE IF NOT EXISTS Module (
     id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     Enseignant_id INTEGER NOT NULL,
     matiere TEXT NOT NULL,
     semestre TEXT NOT NULL,
-    FOREIGN KEY (Enseignant_id) REFERENCES Enseignant(id) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (Enseignant_id) REFERENCES Enseignant(id) 
 );
 
+-- Table Inscrire
 CREATE TABLE IF NOT EXISTS Inscrire (
     module_id INTEGER NOT NULL,
     etudiant_apogee INTEGER NOT NULL,
-    note REAL NOT NULL,
-    valide TEXT NOT NULL
+    note REAL,
+    valide TEXT
     PRIMARY KEY (module_id, etudiant_apogee),
-    FOREIGN KEY (module_id) REFERENCES Module(id) ON UPDATE CASCADE ON DELETE CASCADE,
-    FOREIGN KEY (Etudiant_apogee) REFERENCES Etudiant(num_apogee) ON UPDATE CASCADE ON DELETE CASCADE
+    FOREIGN KEY (module_id) REFERENCES Module(id),
+    FOREIGN KEY (Etudiant_apogee) REFERENCES Etudiant(num_apogee) 
 );
