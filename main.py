7f92d6a7da5fd39dcbd50dbc83485b255785635f
ismail703 <ismailxelm@gmail.com>
2024-12-18 12:27:16 +0100

    upload main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+class switch(object):
+    value = None
+    def __new__(class_, value):
+        class_.value = value
+        return value
+def case(*args):
+    return any((arg == switch.value) for arg in args)
+
+
+while True:
+
+    print("Liste des choix".center(10, "#"))
+    print("1: choix 1")
+    print("2: choix 2")
+    print("3: ")
+    print("4: ")
+    print("5: ")
+    print("0: Quitter")
+    choix = input("Entrer votre choix: ")
+
+    while switch(choix):
+        if case("1"):
+            print("choix 1")
+            break
+        if case("2"):
+            print("choix 2")
+            break
+        if case("3"):
+            print("choix 3")
+            break
+        else:
+           print("invalid choix")
+           break
+
+    if choix == "0":
+        break
+
+
+
